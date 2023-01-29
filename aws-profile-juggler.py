81fc7dac92f876358aab8af291c381e8027fdca5
Osor0s <Osor0s@protonmail.com>
2023-01-29 15:35:19 +0100

    added configure option and fixed output bug

--- aws-profile-juggler.py
+++ aws-profile-juggler.py
@@ -6,11 +6,14 @@ import logging
 import sys
 import re
 
+from os import path
+
 AWS_ACCESS_KEY_ID_ALIASES = ['accesskey', 'accesskeyid', 'awsaccesskeyid', 'awsaccesskey', 'aws access key id', 'aws.access.key.id', 'aws_access_key_id', 'aws-access-key-id', 'aws\-access\-key\-id', 'aws\_access\_key\_id', 'aws\ access\ key\ id', 'aws access key', 'aws.access.key', 'aws_access_key', 'aws-access-key', 'aws\-access\-key', 'aws\_access\_key', 'aws\ access\ key', 'aws key', 'aws.key', 'aws_key', 'aws-key', 'aws\-key', 'aws\_key', 'aws\ key', 'access key', 'access.key', 'access_key', 'access-key', 'access\-key', 'access\_key', 'access\ key', 'aws key id', 'aws.key.id', 'aws_key_id', 'aws-key-id', 'aws\-key\-id', 'aws\_key\_id', 'aws\ key\ id', 'access key id', 'access.key.id', 'access_key_id', 'access-key-id', 'access\-key\-id', 'access\_key\_id', 'access\ key\ id']
 AWS_SECRET_ACCESS_KEY_ALIASES = ['secretaccesskey','awssecretaccesskey', 'secretkeyid','secretkey', 'awssecretkey','awssecretkeyid','aws secret key id', 'aws.secret.key.id', 'aws_secret_key_id', 'aws-secret-key-id', 'aws\-secret\-key\-id', 'aws\_secret\_key\_id', 'aws\ secret\ key\ id', 'aws secret key', 'aws.secret.key', 'aws_secret_key', 'aws-secret-key', 'aws\-secret\-key', 'aws\_secret\_key', 'aws\ secret\ key', 'secret key', 'secret.key', 'secret_key', 'secret-key', 'secret\-key', 'secret\_key', 'secret\ key', 'secret key id', 'secret.key.id', 'secret_key_id', 'secret-key-id', 'secret\-key\-id', 'secret\_key\_id', 'secret\ key\ id', 'aws secret access key', 'aws.secret.access.key', 'aws_secret_access_key', 'aws-secret-access-key', 'aws\-secret\-access\-key', 'aws\_secret\_access\_key', 'aws\ secret\ access\ key', 'secret key id', 'secret.key.id', 'secret_key_id', 'secret-key-id', 'secret\-key\-id', 'secret\_key\_id', 'secret\ key\ id']
 AWS_SESSION_TOKEN_ALIASES = ['aws session token', 'aws.session.token', 'aws_session_token', 'aws-session-token', 'aws\-session\-token', 'aws\_session\_token', 'aws\ session\ token', 'aws token', 'aws.token', 'aws_token', 'aws-token', 'aws\-token', 'aws\_token', 'aws\ token', 'session token', 'session.token', 'session_token', 'session-token', 'session\-token', 'session\_token', 'session\ token', 'token']
 #SEPERATORS = [" ",".","_","-","\-","\_","\ "]
-AWS_CREDENTIALS_FILE = "~/.aws/credentials"
+
+AWS_CREDENTIALS_FILE = path.expanduser("~/.aws/credentials")
 
 def main():
 
@@ -27,6 +30,7 @@ def main():
     parser.add_argument("-p","--profile", help="profile name", type=str,default="temp")
     parser.add_argument("-o","--overwrite", help="overwrite the profile entry if it exists", action="store_true")
     parser.add_argument("-f","--file",help = "AWS credentials file", type=str,default=AWS_CREDENTIALS_FILE)
+    parser.add_argument("-c","--configure",help = "Configure (temp) AWS credentials manually", action="store_true")
 
     args = parser.parse_args()  
 
@@ -47,10 +51,17 @@ def main():
 
     credentials = {}
     # read pipe input
-    if not sys.stdin.isatty():
+
+    if args.configure:
+            configure_credentials(credentials, args.profile)
+            write_credentials(credentials, args.file, args.profile, args.overwrite)
+            sys.exit(0)
+
+    elif not sys.stdin.isatty():
         pipe_input = sys.stdin.read()
         logging.debug("Pipe input:")
         logging.debug(pipe_input)
+
 
         extract_credentials(pipe_input,credentials)
 
@@ -58,6 +69,8 @@ def main():
             print_credentials(credentials,args.profile)
 
         write_credentials(credentials, args.file, args.profile, args.overwrite)
+    else:
+        parser.print_help()
 
         
 
@@ -110,16 +123,18 @@ def write_credentials(credentials, file, profile, overwrite):
 
         if credentials['AWS_ACCESS_KEY_ID'].startswith("AKIA"):
             profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key={credentials["AWS_SECRET_ACCESS_KEY"]}'
-        else:
+        elif credentials['AWS_ACCESS_KEY_ID'].startswith("ASIA"):
             profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key={credentials["AWS_SECRET_ACCESS_KEY"]}\naws_session_token={credentials["AWS_SESSION_TOKEN"]}'
-
+        else:
+            print("[!] No valid AWS Access Key ID. Closing!")
+            sys.exit(-1)
 
         if existing_entry_start == -1:
             #add new profile if name does not exist
             new_content = file_content +"\n"+ profile_string
         else:
             if not overwrite:
-                logging.warning('Profile already exists! Either choose a different profile name with the option "-p" OR use "-o" to overwrite the existing profile')
+                print('[!] Profile already exists! Either choose a different profile name with the option "-p" OR use "-o" to overwrite the existing profile')
                 return
             else:
                 distance_to_next_profile = file_content[existing_entry_start:].find("[")
@@ -130,6 +145,15 @@ def write_credentials(credentials, file, profile, overwrite):
 
         aws_credentials_file.seek(0)
         aws_credentials_file.write(new_content)
+        aws_credentials_file.truncate()
+
+def configure_credentials(credentials, profile):
+    print(f"Manually setting up profile as profile: {profile}")
+    credentials["AWS_ACCESS_KEY_ID"] =      input("AWS Access Key ID:     ")
+    credentials["AWS_SECRET_ACCESS_KEY"] =  input("AWS Secret Access Key: ")
+    if credentials["AWS_ACCESS_KEY_ID"].startswith("ASIA"):
+        print(f"Recognised temporary credentials")
+        credentials["AWS_SESSION_TOKEN"] =  input("AWS Session Token:     ")
 
 if __name__ == "__main__":
     main()
