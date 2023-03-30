6ecbc21cfbe9b8fdbb98e13bd048e1ccaf4331f7
Osor0s <Osor0s@protonmail.com>
2023-03-30 18:15:31 +0200

    refactored configure method & added mfa configure

--- aws-profile-juggler.py
+++ aws-profile-juggler.py
@@ -14,6 +14,7 @@ AWS_SESSION_TOKEN_ALIASES = ['aws session token', 'aws.session.token', 'aws_sess
 #SEPERATORS = [" ",".","_","-","\-","\_","\ "]
 
 AWS_CREDENTIALS_FILE = path.expanduser("~/.aws/credentials")
+AWS_CONFIG_FILE = path.expanduser("~/.aws/config")
 
 def main():
 
@@ -26,11 +27,14 @@ def main():
     group.add_argument("-q", "--quite", help="surpress all output", action="store_true")
     group.add_argument("-d", "--debug", help="See debug output", action="store_true")
     ## dealing with credentials
-    parser.add_argument("--print-creds", help="print out the found credentials", action="store_true")
+    parser.add_argument("--print-creds", help="additionally print out the found credentials to the commandline", action="store_true")
     parser.add_argument("-p","--profile", help="profile name", type=str,default="temp")
     parser.add_argument("-o","--overwrite", help="overwrite the profile entry if it exists", action="store_true")
     parser.add_argument("-f","--file",help = "AWS credentials file", type=str,default=AWS_CREDENTIALS_FILE)
+    ## Options to configure credentials & profiles more easily
     parser.add_argument("-c","--configure",help = "Configure (temp) AWS credentials manually", action="store_true")
+    parser.add_argument("-m","--mfa-configure",help = "Configure an mfa profile to automatically assume a role and ask for new token when necassary", action="store_true")
+    parser.add_argument("--aws-config-file",help = "AWS config file", type=str,default=AWS_CONFIG_FILE)
 
     args = parser.parse_args()  
 
@@ -53,9 +57,13 @@ def main():
     # read pipe input
 
     if args.configure:
-            configure_credentials(credentials, args.profile)
-            write_credentials(credentials, args.file, args.profile, args.overwrite)
-            sys.exit(0)
+        configure_credentials(credentials, args.profile)
+        write_credentials(credentials, args.file, args.profile, args.overwrite)
+        sys.exit(0)
+    
+    if args.mfa_configure:
+        write_mfa_profile(args.profile,args.aws_config_file,args.overwrite)
+        sys.exit(0)
 
     elif not sys.stdin.isatty():
         pipe_input = sys.stdin.read()
@@ -117,17 +125,24 @@ def write_credentials(credentials, file, profile, overwrite):
     """
     Writes the credentials to the ~/.aws/credentials file.
     """
-    with open(file,'r+') as aws_credentials_file:
-        file_content = aws_credentials_file.read()
-        existing_entry_start = file_content.find(profile)
 
-        if credentials['AWS_ACCESS_KEY_ID'].startswith("AKIA"):
-            profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key={credentials["AWS_SECRET_ACCESS_KEY"]}'
-        elif credentials['AWS_ACCESS_KEY_ID'].startswith("ASIA"):
-            profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key={credentials["AWS_SECRET_ACCESS_KEY"]}\naws_session_token={credentials["AWS_SESSION_TOKEN"]}'
-        else:
-            print("[!] No valid AWS Access Key ID. Closing!")
-            sys.exit(-1)
+    # create new profile with the credentials
+    if credentials['AWS_ACCESS_KEY_ID'].startswith("AKIA"):
+        profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key = {credentials["AWS_SECRET_ACCESS_KEY"]}'
+    elif credentials['AWS_ACCESS_KEY_ID'].startswith("ASIA"):
+        profile_string = f'[{profile}]\naws_access_key_id = {credentials["AWS_ACCESS_KEY_ID"]}\naws_secret_access_key = {credentials["AWS_SECRET_ACCESS_KEY"]}\naws_session_token = {credentials["AWS_SESSION_TOKEN"]}'
+    else:
+        print("[!] No valid AWS Access Key ID. Closing!")
+        sys.exit(-1)
+
+    write_new_profile(profile_string, file, profile, overwrite)
+    
+
+def write_new_profile(profile_string, file, profile_start_string, overwrite):
+    # find position for the profile
+    with open(file,'r+') as aws_file:
+        file_content = aws_file.read()
+        existing_entry_start = file_content.find(profile_start_string)
 
         if existing_entry_start == -1:
             #add new profile if name does not exist
@@ -143,17 +158,35 @@ def write_credentials(credentials, file, profile, overwrite):
                 else:
                     new_content =file_content[:existing_entry_start-1]+profile_string+"\n"+file_content[existing_entry_start+distance_to_next_profile:]
 
-        aws_credentials_file.seek(0)
-        aws_credentials_file.write(new_content)
-        aws_credentials_file.truncate()
+        aws_file.seek(0)
+        aws_file.write(new_content)
+        aws_file.truncate()
+
+
 
 def configure_credentials(credentials, profile):
-    print(f"Manually setting up profile as profile: {profile}")
-    credentials["AWS_ACCESS_KEY_ID"] =      input("AWS Access Key ID: ")
-    credentials["AWS_SECRET_ACCESS_KEY"] =  input("AWS Secret Access Key: ")
+    print(f"Manually setting up aws credentials as profile: {profile}")
+    credentials["AWS_ACCESS_KEY_ID"] = input("AWS Access Key ID: ")
+    credentials["AWS_SECRET_ACCESS_KEY"] = input("AWS Secret Access Key: ")
     if credentials["AWS_ACCESS_KEY_ID"].startswith("ASIA"):
         print(f"Recognised temporary credentials")
-        credentials["AWS_SESSION_TOKEN"] =  input("AWS Session Token: ")
+        credentials["AWS_SESSION_TOKEN"] = input("AWS Session Token: ")
+
+
+def write_mfa_profile(profile, file, overwrite):
+    print(f"Manually setting up an MFA profile with the name: {profile}")
+    # Take necessary values for the profile from the user
+    mfa_serial = input("ARN of the MFA device: ")
+    source_profile = input("Profile which is allowed to assume the role: ")
+    role_session_name = input(f"Define a custom 'role session name' (default: {profile}): ")
+    if role_session_name == "":
+        role_session_name = profile
+    role_arn = input(f"Assumeable role which requires MFA: ")
+
+    #prepare profile string and write it to the file
+    profile_string = f"[profile {profile}]\nmfa_serial = {mfa_serial}\nsource_profile = {source_profile}\nrole_session_name = {role_session_name}\nrole_arn = {role_arn}"
+    profile_start_string = f"profile {profile}"
+    write_new_profile(profile_string, file, profile_start_string, overwrite)
 
 if __name__ == "__main__":
     main()
